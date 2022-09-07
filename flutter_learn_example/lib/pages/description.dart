610bbf10a4f15e06e546d7b6c2927ac33c4a1333
Aysenur Perker <nurperker15@gmail.com>
2022-09-08 00:21:03 +0300

    [200~unnecessary usage bug fixed.

--- flutter_learn_example/lib/pages/description.dart
+++ flutter_learn_example/lib/pages/description.dart
@@ -22,80 +22,83 @@ class _MoreInfoPagesState extends State<MoreInfoPages> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.onPrimary,
-      body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Stack(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Stack(
+              children: [
+                Container(
+                  width: 500,
+                  color: Colors.white,
+                  child: Image(
+                    image: widget.images,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(Icons.arrow_back_ios_new_outlined)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
                 children: [
-                  Container(
-                    width: 500,
-                    color: Colors.white,
-                    child: Positioned.fill(
-                      child: Image(
-                        image: widget.images,
-                        fit: BoxFit.fill,
-                      ),
+                  Align(
+                    alignment: AlignAlignment().alignTop,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          widget.characters,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4
+                              ?.copyWith(fontFamily: "ArchivoBlack"),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.favorite_border),
+                        ),
+                      ],
                     ),
                   ),
-                  Positioned(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: IconButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          icon: const Icon(Icons.arrow_back_ios_new_outlined)),
+                  Align(
+                    alignment: AlignAlignment().alignTop,
+                    child: Text(
+                      widget.names,
+                      style: Theme.of(context).textTheme.headline5?.copyWith(
+                            fontSize: 15,
+                          ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Align(
+                    alignment: AlignAlignment().alignTop,
+                    child: Text(
+                      widget.description,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodySmall
+                          ?.copyWith(color: Colors.black),
                     ),
                   ),
                 ],
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: AlignAlignment().alignTop,
-                      child: Text(
-                        widget.characters,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline4
-                            ?.copyWith(fontFamily: "ArchivoBlack"),
-                      ),
-                    ),
-                    Align(
-                      alignment: AlignAlignment().alignTop,
-                      child: Text(
-                        widget.names,
-                        style: Theme.of(context).textTheme.headline5?.copyWith(
-                              fontSize: 15,
-                            ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Align(
-                      alignment: AlignAlignment().alignTop,
-                      child: Text(
-                        widget.description,
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodySmall
-                            ?.copyWith(color: Colors.black),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
