d6bdc9cd6170f1d4b832c169f7572bf9ea572aad
Aysenur Perker <nurperker15@gmail.com>
2022-09-10 01:00:53 +0300

    Favorite page

--- flutter_learn_example/lib/pages/favorite.dart
+++ flutter_learn_example/lib/pages/favorite.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learn_example/pages/home_pages.dart';
 
 class FavoritePage extends StatelessWidget {
   const FavoritePage({Key? key}) : super(key: key);
@@ -6,10 +7,50 @@ class FavoritePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.onPrimary,
       appBar: AppBar(
-        title: const Text("FAVORITE CHARACTERS"),
+        title: const Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Text("FAVORITE CHARACTERS"),
+        ),
       ),
-      body: Column(),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 170,
+            child: ImageAdd(name: ImagePath().nullValue),
+          ),
+          spaceforText(),
+          Text(
+            "No Characters Yet",
+            style: Theme.of(context)
+                .textTheme
+                .headline6
+                ?.copyWith(color: Colors.black.withOpacity(0.6)),
+          ),
+          spaceforText(),
+          SizedBox(
+            width: 300,
+            child: Text(
+              "You did not add any character to your favorite list yet",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline6
+                  ?.copyWith(color: Colors.black.withOpacity(0.4)),
+              //maxLines: 3,
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      )),
+    );
+  }
+
+  SizedBox spaceforText() {
+    return const SizedBox(
+      height: 15,
     );
   }
 }
